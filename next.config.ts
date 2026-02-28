2998f766984be04380e38a88daf5cf3d65abf99e
Deepak Sharma <dasjideepak@gmail.com>
2026-02-28 11:14:09 +0530

    feat: integrate module federation with Next.js, add DashboardApp component, and update dependencies for React and TypeScript

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,27 @@
 import type { NextConfig } from "next";
+import NextFederationPlugin from "@module-federation/nextjs-mf";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
   reactStrictMode: true,
+
+  webpack(config) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "remote1",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./DashboardApp": "./src/components/DashboardApp",
+        },
+        shared: {
+          react: { singleton: true, requiredVersion: false },
+          "react-dom": { singleton: true, requiredVersion: false },
+        },
+        extraOptions: {},
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/components/DashboardApp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DashboardApp() {
+
+  return (
+    <div>
+      <h1>Remote1 App</h1>
+    </div>
+  );
+}
